c1fbc2d39d9d4131ddf2c45178cd923d8324c59c
Amy <amyadzuki@gmail.com>
2018-08-30 00:13:58 +0000

    Fix wrong field names

--- domino.go
+++ domino.go
@@ -9,7 +9,7 @@ func (d Domino) Cause() error {
 
 func (d Domino) Error() string {
 	// could not read config: [read failed: unable to open file or directory] caused: [unable to unmarshal]
-	d.s + ": " + Quotes[0] + d.c.Error() + Quotes[1] + " caused: " + Quotes[0] + d.e.Error() + Quotes[1]
+	d.s + ": " + Quotes[0] + d.cause.Error() + Quotes[1] + " caused: " + Quotes[0] + d.effect.Error() + Quotes[1]
 }
 
 func (d Domino) StackTrace() StackTrace {

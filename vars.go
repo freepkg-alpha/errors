f15b28e4976083521fcc5d7244e41ab70ae7f6b8
Amy <amyadzuki@gmail.com>
2018-08-30 00:10:57 +0000

    Create vars.go

---
+++ vars.go
@@ -0,0 +1,7 @@
+// vars.go: Configuration variables
+// This file is hereby released into the public domain.
+
+package errors
+
+// You can use e.g. `Quotes = [2]string{"\uff62", "\uff63"}` to change
+var Quotes = [2]string{"[", "]"}

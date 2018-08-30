f9ce4115f9cf6ff94f6f5516ee74d34f395d53b8
Amy <amyadzuki@gmail.com>
2018-08-30 00:07:37 +0000

    Create types.go

---
+++ types.go
@@ -0,0 +1,28 @@
+// errors.go: Public types used by this package
+// This file is hereby released into the public domain.
+
+package errors
+
+type Cause struct {
+	s  string
+	st StackTrace
+}
+
+type Causer interface {
+	Cause() error
+}
+
+type Domino struct {
+	cause  error
+	effect error
+	s      string
+	st     StackTrace
+}
+
+type Frame uintptr
+
+type StackTrace []Frame
+
+type StackTracer interface {
+	StackTrace() StackTrace
+}
